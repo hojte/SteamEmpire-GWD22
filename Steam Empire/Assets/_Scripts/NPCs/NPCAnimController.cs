d5a9f72de9d98f3dc03a576c5734d6cc18dae201
Hafrass <sindreva3@gmail.com>
2022-03-25 12:45:51 +0100

    Animated npcs

---
+++ Steam Empire/Assets/_Scripts/NPCs/NPCAnimController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class NPCAnimController : MonoBehaviour
+{
+    enum Animations { WashingClothes, Talking, Crying, Begging, Puking, ArmsCrossed, Writing, FixingCart}
+    [SerializeField] Animations animations;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetFloat("AnimIndex", (int)animations);
+        transform.rotation = Quaternion.Euler(90, transform.rotation.eulerAngles.y, 0);
+    }
+}
