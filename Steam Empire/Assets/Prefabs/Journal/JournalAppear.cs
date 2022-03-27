652d90e97eb10b33846e0c5187cc355b826780d9
fpil <pilfrederik@gmail.com>
2022-03-27 12:19:12 +0200

    Added txt file

--- Steam Empire/Assets/Prefabs/Journal/JournalAppear.cs
+++ Steam Empire/Assets/Prefabs/Journal/JournalAppear.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class JournalAppear : MonoBehaviour
@@ -12,6 +15,7 @@ public class JournalAppear : MonoBehaviour
 
     private void Start()
     {
+        _journalCanvas = GameObject.Find("DiaryCanvas").GetComponent<Canvas>();
         _journalCanvas.enabled = false;
     }
 
@@ -21,6 +25,29 @@ public class JournalAppear : MonoBehaviour
             if (Input.GetKeyDown("tab"))
             {
                 _journalCanvas.enabled = !_journalCanvas.enabled;
+                ReadString(_journalCanvas);
             }
+            
+    }
+    
+    [MenuItem("Tools/Read file")]
+    static void ReadString(Canvas journalCanvas)
+    {
+        string path = "Assets/Prefabs/Journal/journal.txt";
+        //Read the text from directly from the test.txt file
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                String[] lines = line.Split(':');
+                journalCanvas.transform.Find(lines[0]).GetComponent<Text>().enabled = bool.Parse(lines[1]);
+                journalCanvas.transform.Find(lines[0]+" line").GetComponent<Text>().enabled = bool.Parse(lines[2]);
+                Debug.Log(line);
+                
+            }
+            //reader.Close();
+        }
+
     }
 }
