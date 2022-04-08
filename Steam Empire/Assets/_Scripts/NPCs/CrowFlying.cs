10729d66b3fc99af689abaae7c7f48e12eeb74aa
mhooITU <mhoo@itu.dk>
2022-04-08 09:13:38 +0200

    bird flap script

--- Steam Empire/Assets/_Scripts/NPCs/CrowFlying.cs
+++ Steam Empire/Assets/_Scripts/NPCs/CrowFlying.cs
@@ -12,10 +12,15 @@ public class CrowFlying : MonoBehaviour
     Animator anim;
     Vector3 targetPos;
     bool isFlying = false;
+    public AudioClip birdFlapClip;
+    private AudioSource _audioSource;
 
     void Awake()
     {
         anim = GetComponent<Animator>();
+        _audioSource = gameObject.AddComponent<AudioSource>();
+        _audioSource.clip = birdFlapClip;
+        _audioSource.spatialBlend = 1;
     }
 
     void Update()
@@ -37,6 +42,7 @@ public class CrowFlying : MonoBehaviour
             targetPos = transform.position + new Vector3(Random.Range(-flyRadius, flyRadius), flyHeight, Random.Range(-flyRadius, flyRadius));
             isFlying = true;
             anim.SetBool("Flying", true);
+            _audioSource.Play();
         }
     }
 }
