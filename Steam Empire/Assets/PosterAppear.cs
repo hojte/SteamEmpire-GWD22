e143e09f60247e032a0bb17f670e7b4a2700ea78
fpil <pilfrederik@gmail.com>
2022-04-08 11:24:28 +0200

    Added interact text

--- Steam Empire/Assets/PosterAppear.cs
+++ Steam Empire/Assets/PosterAppear.cs
@@ -9,16 +9,38 @@ public class PosterAppear : MonoBehaviour
     [SerializeField]
     private Image _Image;
 
+    private Text _interactText;
+
+
     private void Start()
     {
+        _interactText = GameObject.Find("UIInteract").GetComponent<Text>();
+        _interactText.enabled = false;
         _Image.enabled = false;
     }
 
     void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Player"))
         {
-            _Image.enabled = true; 
+            _interactText.enabled = true;
+            
+
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKey("e")) {
+                _Image.enabled = true;
+                _interactText.enabled = false;
+
+            }
+
         }
     }
 
@@ -26,6 +48,7 @@ public class PosterAppear : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            _interactText.enabled = false;
             _Image.enabled = false; 
         }
     }
