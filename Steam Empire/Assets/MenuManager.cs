0f9045d6c26354dc1ccb154f3ee724127dae5810
javiercdag <javiercdag@gmail.com>
2022-04-13 21:13:26 +0200

    main menu v01

---
+++ Steam Empire/Assets/MenuManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            gameObject.SetActive(false);
+            Cursor.visible = false;
+            FindObjectOfType<PlayerControl>().enablePlayerControls();
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
